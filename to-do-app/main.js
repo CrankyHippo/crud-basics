a5283d0bb72878a0d031daeb9436981f44c39c9e
CrankyHippo <crankyhippo@gmail.com>
2022-08-23 13:01:44 -0700

    update

---
+++ to-do-app/main.js
@@ -0,0 +1,21 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
+//Event listeners
+
+//Functions
+
+let formValidation = () => {};
+let data = [{}];
+let acceptData = () => {};
+let createTask = () => {};
+let deleteTask = () => {};
+let editTask = () => {};
+let resetForm = () => {};
+
+// Anonomous helper function
