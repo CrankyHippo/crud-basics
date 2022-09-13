80f057644b7e79e2930a802b2217b190f96c13f9
CrankyHippo <crankyhippo@gmail.com>
2022-09-13 11:07:51 -0700

    update

--- to-do-app/main.js
+++ to-do-app/main.js
@@ -7,12 +7,33 @@ let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
 
 //Event listeners
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
+});
 
 //Functions
 
-let formValidation = () => {};
+let formValidation = () => {
+  if (textInput.value === "") {
+    console.log("Text input is empty");
+  } else {
+    console.log("success");
+    msg.innerHTML = "Success";
+    acceptData();
+  }
+};
+
 let data = [{}];
-let acceptData = () => {};
+
+let acceptData = () => {
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
+};
+
 let createTask = () => {};
 let deleteTask = () => {};
 let editTask = () => {};
